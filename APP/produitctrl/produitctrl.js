e44453a1b29f6bcb986f61fb8d474f9e09723f36
olivier <olivierrigaut@gmail.com>
2020-08-24 15:06:44 +0200

    bonne pratique instance controlleur

--- APP/produitctrl/produitctrl.js
+++ APP/produitctrl/produitctrl.js
@@ -1,6 +1,11 @@
 angular.module('app').controller('produitctrl', ['$scope', function($scope)
 {
-    $scope.ref='ABD-BCD';
-    $scope.prix=2.20;
-    $scope.nom='Bouffe';
+//    $scope.ref='ABD-BCD';
+//    $scope.prix=2.20;
+//    $scope.nom='Bouffe';
+
+    this.ref='ABD-BCD';
+    this.prix=2.20;
+    this.nom='Bouffe';
+
 }])
